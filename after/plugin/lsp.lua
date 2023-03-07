e812482778f871aac04a248e5fca54496da3478c
Roman Khundadze <roman.wiesel.khundadze@gmail.com>
2023-03-07 23:44:59 +0100

    edit

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,19 +2,7 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'sumneko_lua',
   'rust_analyzer',
-})
-
--- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
 })
 
 local cmp = require('cmp')
